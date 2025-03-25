9036fa9264d1e1b5577ba9c1b6d122df698cc7f4
lili-the-animator <liliansanchez1616@gmail.com>
2025-03-24 20:00:58 -0700

    Restart
    Deleted files to restart the file creation process within GitHub to get the page to work.

--- root/js/top.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-//Click event: scrolls page back to top
-let btn = document.getElementById("myBtn");
-btn.addEventListener("click", () => {
-	document.documentElement.scrollTo({
-		top: 0,
-		behavior: "smooth",
-	})
-})
